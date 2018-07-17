9c28e28d6340ac2904429706b26938e8a48bcc4d
JordanMichele <jordanmichele32@gmail.com>
2018-07-17 18:10:36 -0400

    updated files

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
+
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log('Server has started')
+app.get('/ajaxcall', function(req, res){
+  let data =
+    {
+    firstName: 'Jordan',
+    lastName: 'Michele',
+    message: 'Hello World'
+    };
+  res.send(data)
 });
+
+app.listen(8000, function(){
+  console.log("Server Started")
+})
--- test/mvpTest.js
+++ test/mvpTest.js
@@ -3,18 +3,19 @@
 const chai                = require('chai');
 const chaiHttp            = require('chai-http');
 const { app } = require('../app');
+
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-describe('app.js', function() {
+describe('public', function() {
 
 // note the use of nested `describe` blocks.
 // this allows us to make clearer, more discrete tests that focus
 // on proving something small
 
-  it('check status code', function() {
+  it('keys', function() {
 
-        expect(res).to.have.status(200);
+        expect('/ajaxcall').to.have.key('firstName');
         // otherwise our db seeding didn't work
 
     });
